8a17fd505270799496aabf5a9e410fcaf8ab6c80
Antonio Casoli <voxeldoodle@gmail.com>
2022-10-25 10:31:50 +0200

    Temporal HDFS

---
+++ data_preprocess/preprocess_temporal_HDFS.py
@@ -0,0 +1,146 @@
+import os
+import re
+import pickle
+import argparse
+import pandas as pd
+import numpy as np
+from utils import decision, json_pretty_dump
+from collections import OrderedDict, defaultdict
+
+
+seed = 42
+np.random.seed(seed)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--train_anomaly_ratio", default=1.0, type=float)
+
+params = vars(parser.parse_args())
+
+time_range = 60
+
+data_name = f'hdfs_{params["train_anomaly_ratio"]}_{time_range}s_tar'
+data_dir = "../data/processed/temporal_HDFS"
+
+params = {
+    "log_file": "C:/Users/a.casoli/Desktop/Tesi/Resources/HDFS_result/HDFS_main_structured.csv",
+    "time_range": time_range,
+    "label_file":"C:/Users/a.casoli/Desktop/Tesi/Resources/HDFS/anomaly_label.csv",
+    "test_ratio": 0.2,
+    "random_sessions": True,  # shuffle sessions
+    "train_anomaly_ratio": params["train_anomaly_ratio"],
+}
+
+data_dir = os.path.join(data_dir, data_name)
+os.makedirs(data_dir, exist_ok=True)
+
+
+def preprocess_hdfs(
+    log_file,
+    time_range,
+    label_file,
+    test_ratio=None,
+    train_anomaly_ratio=1,
+    random_sessions=False,
+    **kwargs
+):
+    """Load HDFS structured log into train and test data
+
+    Arguments
+    ---------
+        TODO
+
+    Returns
+    -------
+        TODO
+    """
+    print("Loading HDFS logs from {}.".format(log_file))
+    struct_log = pd.read_csv(log_file, engine="c", na_filter=False, memory_map=True)
+    struct_log["time"] = pd.to_datetime(
+    struct_log.Date.astype(str).str.zfill(6)+
+    struct_log.Time.astype(str).str.zfill(6),
+    format="%d%m%y%H%M%S")
+    struct_log["seconds_since"] = (
+        (struct_log["time"] - struct_log["time"][0]).dt.total_seconds().astype(int)
+    )
+
+    # assign labels
+    label_data = pd.read_csv(label_file, engine="c", na_filter=False, memory_map=True)
+    label_data["Label"] = label_data["Label"].map(lambda x: int(x == "Anomaly"))
+    label_data_dict = dict(zip(label_data["BlockId"], label_data["Label"]))
+
+    session_dict = OrderedDict()
+    column_idx = {col: idx for idx, col in enumerate(struct_log.columns)}
+    for idx, row in enumerate(struct_log.values):
+        current = row[column_idx["seconds_since"]]
+        if idx == 0:
+            sessid = current
+        elif current - sessid > time_range:
+            sessid = current
+        if sessid not in session_dict:
+            session_dict[sessid] = defaultdict(list)
+        session_dict[sessid]["templates"].append(row[column_idx["EventTemplate"]])
+
+        blkId_list = re.findall(r"(blk_-?\d+)", row[column_idx["Content"]])
+        blkId_set = set(blkId_list)
+        session_dict[sessid]["label"].append(0)
+        for blk_Id in blkId_set:
+            if label_data_dict[blk_Id]:
+                session_dict[sessid]["label"][-1] = 1
+                break
+
+    session_idx = list(range(len(session_dict)))
+    # split data
+    if random_sessions:
+        print("Using random partition.")
+        np.random.shuffle(session_idx)
+
+    session_ids = np.array(list(session_dict.keys()))
+
+    train_lines = int((1 - test_ratio) * len(session_idx))
+    test_lines = int(test_ratio * len(session_idx))
+
+    session_idx_train = session_idx[0:train_lines]
+    session_idx_test = session_idx[-test_lines:]
+
+    session_id_train = session_ids[session_idx_train]
+    session_id_test = session_ids[session_idx_test]
+
+    print("Total # sessions: {}".format(len(session_ids)))
+
+    session_train = {
+        k: session_dict[k]
+        for k in session_id_train
+        if (sum(session_dict[k]["label"]) == 0)
+        or (sum(session_dict[k]["label"]) > 0 and decision(train_anomaly_ratio))
+    }
+
+    session_test = {k: session_dict[k] for k in session_id_test}
+
+    # breakpoint()
+
+    session_labels_train = [
+        1 if sum(v["label"]) > 0 else 0 for _, v in session_train.items()
+    ]
+    session_labels_test = [
+        1 if sum(v["label"]) > 0 else 0 for _, v in session_test.items()
+    ]
+
+    train_anomaly = 100 * sum(session_labels_train) / len(session_labels_train)
+    test_anomaly = 100 * sum(session_labels_test) / len(session_labels_test)
+
+    print("# train sessions: {} ({:.2f}%)".format(len(session_train), train_anomaly))
+    print("# test sessions: {} ({:.2f}%)".format(len(session_test), test_anomaly))
+
+    with open(os.path.join(data_dir, "session_train.pkl"), "wb") as fw:
+        pickle.dump(session_train, fw)
+    with open(os.path.join(data_dir, "session_test.pkl"), "wb") as fw:
+        pickle.dump(session_test, fw)
+    json_pretty_dump(params, os.path.join(data_dir, "data_desc.json"))
+
+    print("Saved to {}".format(data_dir))
+    return session_train, session_test
+
+
+if __name__ == "__main__":
+    preprocess_hdfs(**params)
